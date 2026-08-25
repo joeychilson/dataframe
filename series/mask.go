502894a3f74278f38943fdb813ddf6bf2615b3d3
Joey Chilson <joeychilson@outlook.com>
2026-08-24 20:24:39 -0500

    refactor(series): simplify boolean mask conversion

--- series/mask.go
+++ series/mask.go
@@ -6,9 +6,6 @@ import "github.com/joeychilson/dataframe/mask"
 // values select their rows; false and null values do not. The returned Mask
 // has the same length as values.
 func AsMask(values Series[bool]) mask.Mask {
-	if !values.validity.Initialized() {
-		return mask.New(values.values)
-	}
 	nullCount := values.NullCount()
 	if nullCount == 0 {
 		return mask.New(values.values)
